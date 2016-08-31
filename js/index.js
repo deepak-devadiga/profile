5edba8b150220c46a505058c401d708bcb8da500
Deepak K Bhaskar <deepucsait@gmail.com>
2016-08-31 17:20:15 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,10 +9,10 @@ function showPage() {
 }
 	
 $(document).ready(function(){
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	/*if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		console.log('Mobile');
 		 $('body').html("<div style='text-align:center'><p>Sorry...!!!</p><p>This website is not supported on mobiles and tablets.</p><p>Kindly open this site on desktop.</p></div>");
-		}
+		}*/
 	 
 $('.start').click(function(){
 	 $('html, body').animate({
